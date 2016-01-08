529b51ac4574803310235f1c37cc6fa9ce89c9ec
Joe <hover2pi@gmail.com>
2016-01-08 18:00:30 -0500

    Fixed import error. Needs too much application memory! I'll fix it.

--- aperture_phot.py
+++ aperture_phot.py
@@ -5,6 +5,8 @@ from astropy.wcs import WCS
 from astropy.visualization import scale_image
 import photutils
 from astropy.stats import sigma_clipped_stats
+from astropy.visualization import SqrtStretch
+from astropy.visualization.mpl_normalize import ImageNormalize
 
 '''
 Example:
@@ -74,7 +76,7 @@ class exposure:
     # Plot the sources
     if plot:
       positions = (sources['xcentroid'], sources['ycentroid'])
-      apertures = CircularAperture(positions, r=4.)
+      apertures = photutils.CircularAperture(positions, r=4.)
       norm = ImageNormalize(stretch=SqrtStretch())
       plt.imshow(data, cmap='Greys', origin='lower', norm=norm)
       apertures.plot(color='blue', lw=1.5, alpha=0.5)
